8e4f3f524252ecc027a0d4e261710d2f59f02484
Ritikdotcom <ritikdhakate3@gmail.com>
2021-05-09 17:05:03 +0530

    Create login and Register UI

--- app/src/main/java/com/adintech/farmersguide/views/activities/LoginActivity.java
+++ app/src/main/java/com/adintech/farmersguide/views/activities/LoginActivity.java
@@ -3,17 +3,34 @@ package com.adintech.farmersguide.views.activities;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.adintech.farmersguide.R;
 import com.adintech.farmersguide.databinding.ActivityLoginBinding;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
     private ActivityLoginBinding mActivityLoginBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mActivityLoginBinding= DataBindingUtil.setContentView(this,R.layout.activity_login);
+        Initialize();
+    }
+
+    private void Initialize() {
+        mActivityLoginBinding.Register.setOnClickListener(this::onClick);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+        switch (v.getId()){
+            case R.id.Register: intent = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
--- app/src/main/java/com/adintech/farmersguide/views/activities/RegisterActivity.java
+++ app/src/main/java/com/adintech/farmersguide/views/activities/RegisterActivity.java
@@ -1,16 +1,36 @@
 package com.adintech.farmersguide.views.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.adintech.farmersguide.R;
+import com.adintech.farmersguide.databinding.ActivityRegisterBinding;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+    private ActivityRegisterBinding mActivityRegisterBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        mActivityRegisterBinding = DataBindingUtil.setContentView(this,R.layout.activity_register);
+        Initialize();
+    }
+
+    private void Initialize() {
+        mActivityRegisterBinding.AlAccount.setOnClickListener(this::onClick);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+        switch (v.getId()){
+            case R.id.AlAccount: intent = new Intent(RegisterActivity.this,LoginActivity.class);
+            startActivity(intent);
+            break;
+        }
     }
 }
